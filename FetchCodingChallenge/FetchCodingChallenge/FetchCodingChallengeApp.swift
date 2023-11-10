96b506de4ed210b2472556ef80587c49d47de481
Davy <dchuon@ucdavis.edu>
2023-11-09 16:56:15 -0800

    Starting coding challenge, fresh template

---
+++ FetchCodingChallenge/FetchCodingChallenge/FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallenge
+//
+//  Created by Davy Chuon on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
